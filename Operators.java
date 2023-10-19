2aae5d84d1f0deeecec6f4842d8d1bbc5cdcb9c3
Thrishank123 <125119917+Thrishank123@users.noreply.github.com>
2023-10-19 23:19:35 +0530

    Day 2: Operators

---
+++ Operators.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+    // Write your code here
+    double x,y;
+    x= (meal_cost/100.0)*((double)tip_percent);
+    y= (meal_cost/100.0)*((double)tax_percent);
+    meal_cost=meal_cost+x+y;
+    Math.round(meal_cost);
+    System.out.println(Math.round(meal_cost));
+    }
+
+}
+
+public class Operators {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+}
